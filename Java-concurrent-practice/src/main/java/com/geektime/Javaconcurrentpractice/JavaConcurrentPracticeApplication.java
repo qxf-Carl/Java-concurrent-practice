fc6676222e989ae93240daf860bd83f33d15fd61
qingyi.qxf <quxiaofeng@hundunyun.com.cn>
2021-01-14 15:41:32 +0800

    init

---
+++ Java-concurrent-practice/src/main/java/com/geektime/Javaconcurrentpractice/JavaConcurrentPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.geektime.Javaconcurrentpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrentPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrentPracticeApplication.class, args);
+    }
+
+}
---
+++ Java-concurrent-practice/src/test/java/com/geektime/Javaconcurrentpractice/JavaConcurrentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektime.Javaconcurrentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrentPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
